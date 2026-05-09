a9160e003ae7c0d7d8c523e6b07b754b67dda8ed
lglducci <lglducci@hotmail.com>
2026-05-09 05:38:37 -0300

    ok

--- src/components/Sidebaraaa.jsx
+++
@@ -1,4 +0,0 @@
- export default function Sidebar() {
-  alert("entrei no sidebar");
-  return <div>Sidebar OK</div>;
-}
--- src/pages/LancamentoRapido.jsx
+++
@@ -1,8 +0,0 @@
- import LancamentoRapidoDesktop from "./LancamentoRapidoDesktop";
-import LancamentoRapidoMobile from "./LancamentoRapidoMobile";
-
-export default function LancamentoRapido() {
-  const isMobile = window.innerWidth < 768;
-   return isMobile ? <LancamentoRapidoDesktop /> : <LancamentoRapidoDesktop />;
- // return isMobile ? <LancamentoRapidoMobile /> : <LancamentoRapidoDesktop />;
-}
